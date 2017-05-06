516a4a0cd68879a12baf9a2b7b2077b650b71686
Tim McIver <tim.mciver@maxwellhealth.com>
2017-05-06 14:37:54 -0400

    Added @todo about return an Either from `Entity.applyEvent`.

--- src/main/scala/com/maxwellhealth/sourcery/Entity.scala
+++ src/main/scala/com/maxwellhealth/sourcery/Entity.scala
@@ -1,5 +1,9 @@
 package com.maxwellhealth.sourcery
 
 trait Entity[I] extends Identifiable[I] {
+
+  // @todo Consider returning Either[Error, Entity[I]] since there is the
+  // possibility of error: namely that the entity ID in the event may not match
+  // the ID of this entity.
   def applyEvent(event: Event[I]): Entity[I]
 }
